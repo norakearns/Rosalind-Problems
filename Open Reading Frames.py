df2038d3ed25a6e67cfe146fd696910c83e4803e
Nora Kearns <59736592+norakearns@users.noreply.github.com>
2023-06-04 10:27:03 -0700

    Update Open Reading Frames.py

--- Open Reading Frames.py
+++ Open Reading Frames.py
@@ -11,25 +11,21 @@ from Bio import Alphabet
 from Bio.Seq import IUPAC
 '''
 
-# mRNA = translate(Seq)
-# print(mRNA)
 from Bio import SeqIO
 from Bio import Alphabet
 from Bio.Seq import Seq
 for record in SeqIO.parse("C:\\Users\\Nora Kearns\\Downloads\\rosalind_orf (3).txt",'fasta'):
     for_sequence = record.seq
-print(for_sequence)
-
-rev_sequence = for_sequence.reverse_complement()
-print(rev_sequence)
-import re
-# matches = re.findall('(GGG)(.+)(CCCC)', str(for_sequence))
-#matches = re.findall("ATG", str(for_sequence))
-pattern = re.compile(r'(?=(ATG(?:...)*?)(?=TAG|TGA|TAA))')
-matches = re.findall(pattern, str(for_sequence))
-rev_matches = re.findall(pattern, str(rev_sequence))
-print(matches)
-print(rev_matches)
+ rev_sequence = for_sequence.reverse_complement()
+ print(rev_sequence)
+ import re
+ # matches = re.findall('(GGG)(.+)(CCCC)', str(for_sequence))
+ #matches = re.findall("ATG", str(for_sequence))
+ pattern = re.compile(r'(?=(ATG(?:...)*?)(?=TAG|TGA|TAA))')
+ matches = re.findall(pattern, str(for_sequence))
+ rev_matches = re.findall(pattern, str(rev_sequence))
+ print(matches)
+ print(rev_matches)
 
 from Bio import Seq
 for i in matches:
