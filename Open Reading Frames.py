f36f6da34797665ab9228d04a77204fc04898d0c
Nora Kearns <59736592+norakearns@users.noreply.github.com>
2023-06-04 10:26:41 -0700

    Update Open Reading Frames.py

--- Open Reading Frames.py
+++ Open Reading Frames.py
@@ -1,26 +1,15 @@
+'''
+Given: A DNA string s
+ of length at most 1 kbp in FASTA format.
+
+Return: Every distinct candidate protein string that can be translated from ORFs of s
+. Strings can be returned in any order.
+
 import Bio
 from Bio import Seq
 from Bio import Alphabet
 from Bio.Seq import IUPAC
-#
-# codons = {
-#     'UUU': 'F',     'CUU': 'L',     'AUU': 'I',     'GUU': 'V',
-#     'UUC': 'F',     'CUC': 'L',     'AUC': 'I',     'GUC': 'V',
-#     'UUA': 'L',     'CUA': 'L',     'AUA': 'I',     'GUA': 'V',
-#     'UUG': 'L',     'CUG': 'L',     'AUG': 'M',     'GUG': 'V',
-#     'UCU': 'S',     'CCU': 'P',     'ACU': 'T',     'GCU': 'A',
-#     'UCC': 'S',     'CCC': 'P',     'ACC': 'T',     'GCC': 'A',
-#     'UCA': 'S',     'CCA': 'P',     'ACA': 'T',     'GCA': 'A',
-#     'UCG': 'S',     'CCG': 'P',     'ACG': 'T',     'GCG': 'A',
-#     'UAU': 'Y',     'CAU': 'H',     'AAU': 'N',     'GAU': 'D',
-#     'UAC': 'Y',     'CAC': 'H',     'AAC': 'N',     'GAC': 'D',
-#     'UAA': 'Stop',  'CAA': 'Q',     'AAA': 'K',     'GAA': 'E',
-#     'UAG': 'Stop',  'CAG': 'Q',     'AAG': 'K',     'GAG': 'E',
-#     'UGU': 'C',     'CGU': 'R',     'AGU': 'S',     'GGU': 'G',
-#     'UGC': 'C',     'CGC': 'R',     'AGC': 'S',     'GGC': 'G',
-#     'UGA': 'Stop',  'CGA': 'R',     'AGA': 'R',     'GGA': 'G',
-#     'UGG': 'W',     'CGG': 'R',     'AGG': 'R',     'GGG': 'G'
-# }
+'''
 
 # mRNA = translate(Seq)
 # print(mRNA)
